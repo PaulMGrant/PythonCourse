5699381d99256836cd816d528245018e87e1d623
Paul M Grant <paul@paul-grant.co.uk>
2026-01-01 12:23:36 +0000

    V2.0

--- Strings.py
+++ Strings.py
@@ -2,3 +2,7 @@
 #Date: 2025-12-31
 #Description: This module contains utility functions for string manipulation.
 
+mystring = "Hello, World!"
+mystring[0]  # Accessing the first character 'H'
+mystring[9]  # Accessing the tenth character 'r'
+mystring[-2]  # Accessing the second last character 'l'
